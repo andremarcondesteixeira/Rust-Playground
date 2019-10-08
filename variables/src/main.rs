f52c7610b3a95954fdcb39c49c21d09ba65a5e6c
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2019-10-08 01:10:00 -0300

    studying integer types max values

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,28 +1,67 @@
-const MAX_POINTS: u32 = 1_000_000;
-
 fn main() {
-    println!("Max points: {}", MAX_POINTS);
-    let mut x = 5;
-    println!("The value of x is: {}", x);
-    x = 6;
-    println!("The value of x is: {}", x);
-    let something = "abcde";
-    println!("something: {}", something);
-    let something = something.len();
-    println!("something: {}", something);
+    println!("i8 max value: {}, {}", get_signed_integer_max_value(8u8) as i8, std::i8::MAX);
+    println!("i16 max value: {}, {}", get_signed_integer_max_value(16u8) as i16, std::i16::MAX);
+    println!("i32 max value: {}, {}", get_signed_integer_max_value(32u8) as i32, std::i32::MAX);
+    println!("i64 max value: {}, {}", get_signed_integer_max_value(64u8) as i64, std::i64::MAX);
+    println!("i128 max value: {}", std::i128::MAX); // can't calculate this value using the get_signed_integer_max_value function because the max value would be overflown by 1
 
-    println!("i8 max value: {}", get_signed_integer_max_value(8) as i8);
-    println!("i16 max value: {}", get_signed_integer_max_value(16) as i16);
-    println!("i32 max value: {}", get_signed_integer_max_value(32) as i32);
-    println!("i64 max value: {}", get_signed_integer_max_value(64) as i64);
-    println!("i128 max value: {}", std::i128::MAX);
+    println!("i8 min value: {}, {}", get_signed_integer_min_value(8u8) as i8, std::i8::MIN);
+    println!("i16 min value: {}, {}", get_signed_integer_min_value(16u8) as i16, std::i16::MIN);
+    println!("i32 min value: {}, {}", get_signed_integer_min_value(32u8) as i32, std::i32::MIN);
+    println!("i64 min value: {}, {}", get_signed_integer_min_value(64u8) as i64, std::i64::MIN);
+    println!("i128 min value: {}, {}", get_signed_integer_min_value(128u8) as i128, std::i128::MIN);
 
-    let _a = 57u8;
+    println!("u8 max value: {}, {}", get_unsigned_integer_max_value(8u8) as u8, std::u8::MAX);
+    println!("u16 max value: {}, {}", get_unsigned_integer_max_value(16u8) as u16, std::u16::MAX);
+    println!("u32 max value: {}, {}", get_unsigned_integer_max_value(32u8) as u32, std::u32::MAX);
+    println!("u64 max value: {}, {}", get_unsigned_integer_max_value(64u8) as u64, std::u64::MAX);
+    println!("u128 max value: {}", std::u128::MAX); // can't calculate this value using the get_unsigned_integer_max_value function because the max value would be overflown by 1
 }
 
+/* Each signed variant can store numbers from -(2^(n-1)) to 2^(n-1)-1 inclusive,
+where n is the number of bits that variant uses */
 fn get_signed_integer_max_value(bits: u8) -> i128 {
-    let bits: u8 = bits - 1;
+    let bits: u8 = bits - 1u8;
     let mut value: i128 = 2;
+    let mut count: u8 = 1;
+
+    loop {
+        count = count + 1u8;
+        value = value * 2i128;
+        if count == bits {
+            break;
+        }
+    }
+
+    return value - 1i128;
+}
+
+/* Each signed variant can store numbers from -(2^(n-1)) to 2^(n-1)-1 inclusive,
+where n is the number of bits that variant uses */
+fn get_signed_integer_min_value(bits: u8) -> i128 {
+    // When 1 is added to a integer type's maximum value, the result is the type's minimum value,
+    // so, could just do this if the amount of bits is less than 128 (because otherwise the program
+    // would crash at runtime)
+    // return get_signed_integer_max_value(bits) + 1i128;
+
+    let bits: u8 = bits - 1u8;
+    let mut value: i128 = -2;
+    let mut count: u8 = 1;
+
+    loop {
+        count = count + 1u8;
+        value = value * 2i128;
+        if count == bits {
+            break;
+        }
+    }
+
+    return value;
+}
+
+/* Unsigned variants can store numbers from 0 to (2^n)-1 */
+fn get_unsigned_integer_max_value(bits: u8) -> u128 {
+    let mut value: u128 = 2;
     let mut count: u8 = 1;
 
     loop {
@@ -33,5 +72,5 @@ fn get_signed_integer_max_value(bits: u8) -> i128 {
         }
     }
 
-    return value - 1;
+    return value - 1u128;
 }
