b5db06a8dde819c4cf6406767576d8b428083fc7
André Marcondes Teixeira <andre.teixeira@orakolo.com.br>
2019-12-01 06:07:14 -0300

    separating modules into multiple files

---
+++ restaurant/src/front_of_house/hosting.rs
@@ -0,0 +1,6 @@
+pub fn add_to_waitlist() {}
+fn seat_at_table() {}
+
+pub mod hosting_child_module {
+    pub fn foo() {}
+}
--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,25 +1,6 @@
- #[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
-
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-        fn server_order() {}
-        fn take_payment() {}
-    }
-}
-
 fn serve_order() {}
+
+mod front_of_house;
 
 mod back_of_house {
     pub fn fix_incorrect_order() {
@@ -51,6 +32,7 @@ mod back_of_house {
 
 pub fn eat_at_restaurant() {
     crate::front_of_house::hosting::add_to_waitlist();
+    crate::front_of_house::hosting::hosting_child_module::foo();
     front_of_house::hosting::add_to_waitlist();
     back_of_house::fix_incorrect_order();
     let mut meal = back_of_house::Breakfast::summer("Rye");
