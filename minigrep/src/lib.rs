290340fb9e0e76799d586e6bc3c11dc1e759b370
André Marcondes Teixeira <andremarcondesteixeira@gmail.com>
2020-05-31 22:56:49 -0300

    starting chapter 12.4 from Rust book

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,38 @@
+use std::fs;
+use std::error::Error;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    Ok(())
+}
+
+pub struct Config<'a> {
+    query: &'a str,
+    filename: &'a str,
+}
+
+impl<'a> Config<'a> {
+    pub fn new(args: &'a [String]) -> Result<Config<'a>, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+
+        let query: &'a str = &args[1];
+        let filename: &'a str = &args[2];
+        Ok(Config { query, filename })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+    }
+}
