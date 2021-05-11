c1eb7a2d026001901c86af3d996753cc514da23f
André Teixeira <andremarcondesteixeira@gmail.com>
2021-05-10 22:00:29 -0300

    testing filter method of an interator

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -33,10 +33,51 @@ fn calling_next_directly() {
     assert_eq!(counter.next(), Some(4));
     assert_eq!(counter.next(), Some(5));
     assert_eq!(counter.next(), None);
+}
 
+#[test]
+fn testing_the_map_function() {
     let v1: Vec<i32> = vec![1, 2, 3];
     let v2: Vec<i32> = v1.iter().map(|x| x + 1).collect();
     assert_eq!(v2, vec![2, 3, 4]);
+}
+
+#[test]
+fn testing_the_filter_function() {
+    #[derive(PartialEq, Debug)]
+    struct Shoe {
+        size: u8,
+        style: String,
+    }
+    let shoes = vec![
+        Shoe {
+            size: 10,
+            style: String::from("sneaker"),
+        },
+        Shoe {
+            size: 12,
+            style: String::from("sandal"),
+        },
+        Shoe {
+            size: 10,
+            style: String::from("boot"),
+        },
+    ];
+    let my_size = 10_u8;
+    let filtered: Vec<Shoe> = shoes.into_iter().filter(|x| x.size == my_size).collect();
+    assert_eq!(
+        filtered,
+        vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker"),
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot"),
+            }
+        ]
+    );
 }
 
 #[test]
