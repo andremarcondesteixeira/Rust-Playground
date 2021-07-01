c02c63f9e0cb54031d516796bcb284e1a584b4f3
André M. Teixeira <andremarcondesteixeira@gmail.com>
2021-07-01 00:05:59 -0300

    studying threads

--- chapter-16-threads/src/main.rs
+++ chapter-16-threads/src/main.rs
@@ -1,8 +1,7 @@
 use std::thread;
+use std::time::Duration;
 
 fn main() {
-    use std::time::Duration;
-
     let handle = thread::spawn(|| {
         for i in 1..10 {
             println!("From spawned thread: {}", i);
@@ -10,7 +9,6 @@ fn main() {
         }
     });
 
-    
     for i in 1..5 {
         println!("From main thread: {}", i);
         thread::sleep(Duration::from_millis(1));
@@ -20,8 +18,8 @@ fn main() {
 
     using_move_closures_with_threads();
     using_channels();
+    using_mutex();
 }
-
 
 fn using_move_closures_with_threads() {
     let v = vec![1, 2, 3, 4, 5];
@@ -37,9 +35,58 @@ fn using_channels() {
     use std::sync::mpsc;
 
     let (sender, receiver) = mpsc::channel();
+    let another_transmitter = mpsc::Sender::clone(&sender);
 
-    thread::spawn(move || sender.send(String::from("Hello!")).unwrap());
+    thread::spawn(move || {
+        let messages = vec![
+            String::from("hello"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+        for message in messages {
+            sender.send(message).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
 
-    let received = receiver.recv().unwrap();
-    println!("{}", received);
+    thread::spawn(move || {
+        let messages = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+        for message in messages {
+            another_transmitter.send(message).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    //let received = receiver.recv().unwrap();
+    for message in receiver {
+        println!("{}", message);
+    }
+}
+
+fn using_mutex() {
+    use std::sync::{Arc, Mutex};
+
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
